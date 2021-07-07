f71b613bb6c8d9a9e63c0d0126a6356c05fa9f03
Garrido10 <fernando.garrido98@gmail.com>
2021-07-07 20:27:08 -0300

    Atividades 07.07

---
+++ Cliente.java
@@ -0,0 +1,15 @@
+package br.com.generation.cliente;
+
+public class Cliente {
+	
+	//Atributos
+		String nome;
+		String endereco;
+		String estadoCivil;
+		int idade;
+		String telefone;
+		;
+		
+		//Metodos - Ações
+
+}
---
+++ Eletronico.java
@@ -0,0 +1,14 @@
+package br.com.generation.eletronico;
+
+public class Eletronico {
+	
+	//Atributos - Variáveis
+		String nome;
+		String peça;	
+		String marca;
+		String chip;	
+		double preço;
+		
+		//Métodos
+
+}
